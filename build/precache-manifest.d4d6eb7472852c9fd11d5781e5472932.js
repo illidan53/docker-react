b9f7d475d3783a0657c45f8cd3b2770d530af6ef
illidan53 <ubuntu@ip-172-31-0-104.ec2.internal>
2021-04-23 01:16:26 +0000

    initial commit

---
+++ build/precache-manifest.d4d6eb7472852c9fd11d5781e5472932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7f349fdca850b8d86293d28dfb0f35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c41e79565b06bf83b66",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "e88cf915786643f1267f",
+    "url": "/static/js/2.56686953.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.56686953.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c41e79565b06bf83b66",
+    "url": "/static/js/main.f91eefc9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
